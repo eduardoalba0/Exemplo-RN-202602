cd2b9bac1402cdc675640956169f6e6456ac68b6
Eduardo Alba <eduardoalba0@hotmail.com>
2026-09-16 20:42:36 -0300

    feat: add DrawerNavigator component and remove gesture handler wrappers

--- App.js
+++ App.js
@@ -1,17 +1,14 @@
 import 'react-native-gesture-handler';
-import { GestureHandlerRootView } from "react-native-gesture-handler";
 import { PaperProvider } from "react-native-paper";
 import { NavigationContainer } from "@react-navigation/native";
 import DrawerNavigator from "./src/components/DrawerNavigator";
 
 export default function App() {
   return (
-    <GestureHandlerRootView style={{ flex: 1 }}>
-      <NavigationContainer>
-        <PaperProvider>
-          <DrawerNavigator />
-        </PaperProvider>
-      </NavigationContainer>
-    </GestureHandlerRootView>
+    <NavigationContainer>
+      <PaperProvider>
+        <DrawerNavigator />
+      </PaperProvider>
+    </NavigationContainer>
   );
 }
--- src/components/DrawerNavigator.js
+++ src/components/DrawerNavigator.js
@@ -1,12 +1,17 @@
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import StackNavigator from './StackNavigator';
+import { MaterialIcons } from '@expo/vector-icons';
 
 const Drawer = createDrawerNavigator();
 
 function DrawerNavigator() {
     return (
-        <Drawer.Navigator initialRouteName='Principal'>
-            <Drawer.Screen name="Principal" component={StackNavigator} />
+        <Drawer.Navigator initialRouteName='Home'>
+            <Drawer.Screen name="Home" component={StackNavigator}
+                options={{
+                    drawerIcon: () => <MaterialIcons name='home' size={24} color="black" />
+                }}
+            />
         </Drawer.Navigator>
     )
 }
