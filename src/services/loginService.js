fa416667db5f401b9499adcf66243a6ec47a24f4
Eduardo Alba <eduardoalba0@hotmail.com>
2026-09-19 19:33:54 -0300

    feat: cadastro do usuário no db, feedback visual da interface de login e cadastro, Typedefs

--- src/services/loginService.js
+++ src/services/loginService.js
@@ -15,7 +15,7 @@ function useAuthService() {
             usuario.id = userCredential.user.uid;
             const { senha, ...dados } = usuario;
             const docRef = doc(db, COLLECTION_NAME, usuario.id);
-            await setDoc(docRef, dados);
+            await setDoc(docRef, dadosParaSalvar);
         } catch (error) {
             throw error;
         }
@@ -27,7 +27,6 @@ function useAuthService() {
     const loginEmailSenha = async (usuario) => {
         try {
             const userCredential = await signInWithEmailAndPassword(auth, usuario.email, usuario.senha);
-            console.log("Usuário logado:", userCredential.user.uid);
             return userCredential;
         } catch (error) {
             throw error;
